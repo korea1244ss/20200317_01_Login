661e45767af74d7e226269b91562c636241e394b
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-17 09:58:29 +0900

    Initial commit

---
+++ app/src/main/java/Kr/Uk/DH/a20200317_01_login/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package Kr.Uk.DH.a20200317_01_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
